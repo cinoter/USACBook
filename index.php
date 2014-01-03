ee96fb320ca8942cc43a4f7459b51953b72ae481
cinoter <cinoter@hotmail.com>
2014-01-02 20:29:11 -0600

    se estructuro de forma correcta el proyecto

--- index.php
+++ index.php
@@ -7,7 +7,8 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>UsacBook</title>
+        <link href="style/style.css" rel="stylesheet" type="text/css" /> 
     </head>
     <body>
         <!--    PHP PARA EL LOGIN        -->
@@ -53,57 +54,14 @@ if(!isset($_SESSION['userid']))
         } 
     } 
 ?> 
-           <!-- CSS PARA EL FORM LOGIN     -->
-        <style type="text/css"> 
-*{ 
-    font-size: 14px; 
-} 
-body{ 
-background-image: url("usac.jpg"); 
-} 
-form.login { 
-    background: none repeat scroll 0 0 #F1F1F1; 
-    border: 1px solid #DDDDDD; 
-    font-family: sans-serif; 
-    margin: 0 auto; 
-    padding: 20px; 
-    width: 278px; 
-    box-shadow:0px 0px 20px black; 
-    border-radius:10px; 
-} 
-form.login div { 
-    margin-bottom: 15px; 
-    overflow: hidden; 
-} 
-form.login div label { 
-    display: block; 
-    float: left; 
-    line-height: 25px; 
-} 
-form.login div input[type="text"], form.login div input[type="password"] { 
-    border: 1px solid #DCDCDC; 
-    float: right; 
-    padding: 4px; 
-} 
-form.login div input[type="submit"] { 
-    background: none repeat scroll 0 0 #DEDEDE; 
-    border: 1px solid #C6C6C6; 
-    float: right; 
-    font-weight: bold; 
-    padding: 4px 20px; 
-} 
-.error{ 
-    color: red; 
-    font-weight: bold; 
-    margin: 10px; 
-    text-align: center; 
-} 
+        
 </style> 
         <!--         FORM DE LOS COMPONENTES DEL LOGIN         -->
     <form action="" method="post" class="login"> 
     <div><label>Username</label><input name="user" type="text" ></div> 
     <div><label>Password</label><input name="password" type="password"></div> 
-    <div><input name="login" type="submit" value="login"></div> 
+     <div><input name="login" type="submit" value="login"></div> 
+     <a  href="http:registro.php"style="text-decoration:none; color: black"> Registrarse</a>
 </form> 
 <?php
 } else { 
